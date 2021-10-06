4c0ae34975ae1842783cbe4f105fd816621f5a33
Pawel Szwarnowski <pawel.szwarnowski@o2.pl>
2021-10-06 17:21:03 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,67 @@
+import math
+import numpy as np
+
+def cylinder_area(r:float,h:float) -> float:
+    """Obliczenie pola powierzchni walca. 
+    Szczegółowy opis w zadaniu 1.
+    
+    Parameters:
+    r (float): promień podstawy walca 
+    h (float): wysokosć walca
+    
+    Returns:
+    float: pole powierzchni walca 
+    """
+    if r <= 0 or h <= 0:
+        return None
+    else:
+        return 2*math.pi*r*r + 2*math.pi*r*h
+
+def fib(n:int):
+    """Obliczenie pierwszych n wyrazów ciągu Fibonnaciego. 
+    Szczegółowy opis w zadaniu 3.
+    
+    Parameters:
+    n (int): liczba określająca ilość wyrazów ciągu do obliczenia 
+    
+    Returns:
+    np.ndarray: wektor n pierwszych wyrazów ciągu Fibonnaciego.
+    """
+    lista = []
+    if n >=0:
+        if n == 0:
+            lista.append(0)
+        elif n == 1:
+            lista.append(1)
+        else:
+            lista.append(lista[-1] + lista[-2])
+
+
+    return None
+
+def matrix_calculations(a:float):
+    """Funkcja zwraca wartości obliczeń na macierzy stworzonej 
+    na podstawie parametru a.  
+    Szczegółowy opis w zadaniu 4.
+    
+    Parameters:
+    a (float): wartość liczbowa 
+    
+    Returns:
+    touple: krotka zawierająca wyniki obliczeń 
+    (Minv, Mt, Mdet) - opis parametrów w zadaniu 4.
+    """
+    return None
+
+def custom_matrix(m:int, n:int):
+    """Funkcja zwraca macierz o wymiarze mxn zgodnie 
+    z opisem zadania 7.  
+    
+    Parameters:
+    m (int): ilość wierszy macierzy
+    n (int): ilość kolumn macierzy  
+    
+    Returns:
+    np.ndarray: macierz zgodna z opisem z zadania 7.
+    """
+    return None
